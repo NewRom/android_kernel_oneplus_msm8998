e1d464ee4a5bdf93b55212f525cd97830ebe29e1
Jorge Ayres Pereira <jgcaap@gmail.com>
2017-11-25 16:40:13 +0100

    Make compatible with AARCH64 GCC49

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -886,10 +886,10 @@ void log_buf_kexec_setup(void)
 #endif
 
 /* requested log_buf_len from kernel cmdline */
-static unsigned long __initdata new_log_buf_len;
+static unsigned long new_log_buf_len;
 
 /* we practice scaling the ring buffer by powers of 2 */
-static void __init log_buf_len_update(unsigned size)
+static void log_buf_len_update(unsigned size)
 {
 	if (size)
 		size = roundup_pow_of_two(size);
@@ -898,7 +898,7 @@ static void __init log_buf_len_update(unsigned size)
 }
 
 /* save requested log_buf_len since it's too early to process it */
-static int __init log_buf_len_setup(char *str)
+static int log_buf_len_setup(char *str)
 {
 	unsigned size = memparse(str, &str);
 
